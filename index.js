7fa21c956cf7c4485a3a25deb378048dd7cf7c41
R15hav <kumarrishav432@gmail.com>
2023-10-01 23:59:44 +0530

    typing working

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const dataSpyList = document.querySelectorAll('[data-bs-spy="scroll"]')
-dataSpyList.forEach(dataSpyEl => {
-  console.log("call it how many times");
-})
+var idList = ['scrollspyHeading1', 'scrollspyHeading2', 'scrollspyHeading3']
+function initTyping(event) {
+    var eleId=event.target.children[0].id;
+    var h1 = document.getElementById(eleId);
+    h1.classList.add('typing')
+    idList.forEach(element => {
+        if (element != eleId) {
+            var h1 = document.getElementById(element);
+            h1.classList.remove('typing')
+        }
+    });
+}
